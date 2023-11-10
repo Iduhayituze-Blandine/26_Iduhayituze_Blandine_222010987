f99b5f6c47382573dfb65486bd3ce971b083925a
Iduhayituze-Blandine <iduhayituzeblandine@gmail.com>
2023-11-10 17:09:28 +0200

    Inheritance

--- Program1.java
+++ Program1.java
@@ -1,12 +1,3 @@
-class Mathematics{
-    int math_marks (int quiz, int test, int exam){
-        return quiz + test + exam;
-    }
-}
-class Science extends Mathematics {
-    void experiments(int chem, int bios, int phy){
-    }
-}
 class English {
     void level(){
         System.out.println("Proficient");
@@ -31,14 +22,11 @@ public class Program1{
     public static void main (String [] args){
 
         System.out.println("The overall overview \n");
-        Science s = new Science();
         Poetry p = new Poetry();
         Art a = new Art();
         a.level();
         a.drawing();
         p.theater();
         p.poem();
-        s.experiments(45, 47, 34);
-        s.math_marks(20, 30, 40);
     }
 }
---
+++ Subject.java
@@ -0,0 +1,5 @@
+public interface Subject{
+    int Quiz(int quiz);
+    int CAT(int CAT);
+    int Exam(int exam);
+}
